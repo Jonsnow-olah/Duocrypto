e4aaa6843f1f14111c35369441a6eb61ea3c9e0b
olah <olahisrael07@gmail.com>
2024-05-28 02:31:45 +0100

    Implemented drizzle ORM as database

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,9 +1,23 @@
-const CoursesPage = () => {
+import { getCourses, getUserProgress } from "@/db/queries";
+import { List } from "./list";
+import { courses } from "@/db/schema";
+
+const CoursesPage = async () => {
+    const courses = await getCourses();
+    const userProgress = await getUserProgress();
+
     return ( 
-        <div>
-            Courses Page
+        <div className="h-full max-w-[912px] px-3 mx-auto">
+            <h1 className="text-2xl font-bold text-neutral-700">
+                Language Courses
+            </h1>
+            
+            <List 
+                courses={courses}
+                activeCourseId={userProgress?.activeCourseId}
+            />
         </div>
      );
-}
+};
  
 export default CoursesPage;
